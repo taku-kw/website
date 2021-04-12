f2d8566554bcf74b949de055096017f3e03fc3eb
taku-kw <riverbooktable@gmail.com>
2021-04-12 11:23:52 +0900

    add vue.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 // 以下追記
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
@@ -8,4 +10,7 @@ environment.plugins.prepend('Provide',
     })
 )
 // ここまで
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }; // <- add alias
 module.exports = environment
---
+++ test/controllers/session_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class SessionControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get session_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get session_create_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get session_delete_url
+    assert_response :success
+  end
+end
